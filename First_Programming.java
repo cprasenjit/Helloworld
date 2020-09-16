666965ed8881f26fcdf186b95f0982a070db1fd6
cprasenjit <thisisprasenjit@gmail.com>
2020-09-16 08:12:42 +0530

    third commit

--- First_Programming.java
+++ First_Programming.java
@@ -2,7 +2,8 @@ public class First_Programming {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("This is git integration");
+		System.out.println("This is jenkins training");
+		System.out.println("This is first project");
 		
 	}
 
